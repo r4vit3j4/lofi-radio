643873c0143b8abfb5b5525ad9b8a48c024301c0
Ravi Teja <raviteja281003@gmail.com>
2022-03-30 13:35:07 +0530

    Fixed GUI and added some stations

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="bg-dark h-screen">
-    <div class="flex h-[88vh]">
+  <div class="bg-dark h-screen overflow-hidden">
+    <div class="flex h-[90vh]">
       <!-- Side Bar -->
       <div class="w-60 bg-black h-full flex-none flex flex-col hidden lg:block">
-        <div class="p-6">
+        <div class="p-6 flex items-center justify-center">
           <img
             src="https://radio-app-r4vit3j4.netlify.app/img/test.bac0d12e.png"
-            class="invert h-12"
+            class="invert h-[50px]"
           />
           <!-- ./assets/logo/spotify-black.png -->
         </div>
@@ -44,17 +44,19 @@
         </div>
         <div class="flex-1">
           <div class="ml-6 h-full flex flex-col justify-between">
-            <div class="w-full mt-2 h-64 overflow-y-scroll scrl">
+            <div
+              class="w-full mt-2 h-56 overflow-y-scroll overflow-x-hidden scrl"
+            >
               <p
                 v-for="album in albums"
                 :key="album.name"
-                class="text-lightest hover:text-white text-sm py-[6px] whitespace-nowrap"
+                class="text-lightest hover:text-white text-sm py-[6px] whitespace-nowrap line-clamp-1"
               >
                 {{ album.name }}
               </p>
             </div>
 
-            <div class="mb-3">
+            <div class="mt-3">
               <button
                 class="flex items-center justify-start text-white opacity-75 hover:opacity-100"
               >
@@ -138,12 +140,13 @@
             <div
               v-for="item in BestChart"
               :key="item.title"
-              class="p-2 pt-3 cursor-pointer w-96 flex-shrink-0 lg:mb-2"
+              class="p-2 pt-3 w-96 flex-shrink-0 lg:mb-2"
             >
               <div
-                class="hover:bg-light bg-[rgba(40,40,40,0.25)] min-h-[340px] w-full h-auto p-5 pt-4 rounded-lg shadow-md"
+                class="hover:bg-light cursor-pointer transition-all duration-300 bg-[rgba(40,40,40,0.25)] min-h-[340px] w-full h-auto p-5 pt-4 rounded-lg shadow-md"
+                @click="handleClick(item.index)"
               >
-                <img :src="item.src" class="h-auto w-full shadow mb-2" />
+                <img :src="item.src" class="h-[190px] w-full shadow mb-2" />
                 <h1
                   class="text-base font-semibold text-white mt-4 my-3 line-clamp-2"
                 >
@@ -160,7 +163,7 @@
     </div>
     <!-- Play bar -->
     <div
-      class="w-full sticky bottom-0 border-t border-light bg-[#181818] h-[12vh] min-h-[100px] flex items-center justify-between px-6"
+      class="w-full sticky bottom-0 border-t border-light bg-[#181818] h-[10vh] min-h-[80px] flex items-center justify-between px-6"
     >
       <!-- Song Info -->
       <div class="flex items-center flex-shrink-0">
@@ -177,22 +180,32 @@
       </div>
       <!-- Player -->
       <div class="text-lightest flex flex-col items-center">
-        <div class="flex item-center space-x-5 mb-3">
+        <div class="flex items-center space-x-5">
           <i
-            class="ti ti-player-skip-back text-lightest hover:text-white text-xl"
+            class="ti cursor-pointer ti-player-skip-back text-lightest hover:text-white text-xl"
           ></i>
-          <div class="bg-white rounded-full px-[5px]">
-            <i class="ti ti-player-play text-black text-xl"></i>
+
+          <div
+            @click="!isPlaying ? playSong(5) : pauseSong(5)"
+            class="cursor-pointer"
+          >
+            <PlayIcon v-if="!isPlaying" class="text-black invert h-10 w-10" />
+            <!-- <i
+              v-if="!isPlaying"
+              class="ti ti-player-play text-black text-xl"
+            ></i> -->
+            <PauseIcon v-else class="text-black invert h-10 w-10" />
+            <!-- <i v-else class="ti ti-player-pause text-black text-xl"></i> -->
           </div>
           <i
-            class="ti ti-player-skip-forward text-lightest hover:text-white text-xl"
+            class="ti cursor-pointer ti-player-skip-forward text-lightest hover:text-white text-xl"
           ></i>
         </div>
-        <div class="flex text-xs items-center space-x-2">
+        <!-- <div class="flex text-xs items-center space-x-2">
           <p>2:10</p>
           <input type="range" class="h-1 lg:w-96 md:w-72 w-40" />
           <p>7:09</p>
-        </div>
+        </div> -->
       </div>
       <!-- Volume -->
       <div class="flex items-center">
@@ -211,6 +224,7 @@
         <input
           v-model="volume"
           type="range"
+          @input="updateVolume"
           class="w-24 h-[3px] hidden md:inline-block"
         />
       </div>
@@ -219,12 +233,53 @@
 </template>
 
 <script>
+import { PlayIcon, PauseIcon } from "@heroicons/vue/solid";
 export default {
   name: "App",
+  components: {
+    PauseIcon,
+    PlayIcon,
+  },
+  computed: {
+    volume_dec() {
+      return this.volume / 100;
+    },
+  },
+  methods: {
+    handleClick(index) {
+      if (this.isPlaying) {
+        this.pauseSong(index);
+      } else this.playSong(index);
+    },
+    playSong(index) {
+      if (index === 5) {
+        this.player = new Audio(
+          "https://coderadio-relay-blr.freecodecamp.org/radio/8010/low.mp3"
+        );
+        this.player.play();
+        this.player.volume = 0.5;
+        this.isPlaying = true;
+      }
+    },
+    pauseSong(index) {
+      this.player.pause();
+      delete this.player;
+      this.player = null;
+      this.isPlaying = false;
+    },
+    updateVolume() {
+      this.player.volume = this.volume_dec;
+      console.log(this.volume_dec, this.player.volume);
+    },
+  },
   data() {
     return {
+      player: null,
       showDropDown: false,
+      isPlaying: false,
       volume: 50,
+
+      isVolumeShowing: false,
       pages: [
         {
           id: "home",
@@ -283,28 +338,39 @@ export default {
           src: "https://i.ytimg.com/vi/5qap5aO4i9A/hq720.jpg",
           title: "lofi hip hop radio - beats to relax/study to",
           artist: "Lofi Girl",
+          index: 0,
         },
         {
           src: "https://i.ytimg.com/vi/kgx4WGK0oNU/hq720.jpg",
           title:
             "jazz/lofi hip hop radio🌱chill beats to relax/study to [LIVE 24/7]",
           artist: "阿鲍Abao",
+          index: 0,
         },
         {
           src: "https://i.ytimg.com/vi/N65Jb683pXQ/hq720.jpg",
           title:
             "Spinnin' | 24/7 Live Radio | Deep House, Chill House | Dance Music to relax/study to",
           artist: "Spinnin Records",
+          index: 0,
         },
         {
-          src: "https://i.ytimg.com/vi/5qap5aO4i9A/hq720.jpg",
-          title: "lofi hip hop radio - beats to relax/study to",
+          src: "https://i.ytimg.com/vi/WDXPJWIgX-o/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD5ppCZ18qs8ii5MF-XJMZNz6YnDA",
+          title: "anime lofi hip hop radio - 24/7 chill lofi remixes of anime",
           artist: "Lofi Girl",
+          index: 0,
         },
         {
-          src: "https://i.ytimg.com/vi/5qap5aO4i9A/hq720.jpg",
-          title: "lofi hip hop radio - beats to relax/study to",
-          artist: "Lofi Girl",
+          src: "https://i.ytimg.com/vi/ceqgwo7U28Y/hq720.jpg",
+          title: "ＣＨＩＬＬ　ＲＡＤＩＯ ２４／７",
+          artist: "the bootleg boy 2",
+          index: 0,
+        },
+        {
+          src: "https://cdn-media-1.freecodecamp.org/code-radio/bg.jpg",
+          title: "Code Radio 24/7 music designed for coding",
+          artist: "FreeCodeCamp",
+          index: 5,
         },
       ],
     };
@@ -329,5 +395,93 @@ export default {
 }
 .scrl::-webkit-scrollbar-thumb {
   background-color: rgb(128, 128, 128, 0.6);
+}
+
+input[type="range"] {
+  height: 16px;
+  -webkit-appearance: none;
+  background: #181818;
+  margin: 10px 0;
+  /* width: 100%; */
+}
+input[type="range"]:focus {
+  outline: none;
+}
+input[type="range"]::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 3px;
+  cursor: pointer;
+  box-shadow: 0px 0px 0px #000000;
+  background: #1db954;
+  border-radius: 50px;
+  border: 0px solid #000000;
+}
+input[type="range"]::-webkit-slider-thumb {
+  box-shadow: 0px 0px 0px #000000;
+  border: 1px solid #ffffff;
+  height: 10px;
+  width: 10px;
+  border-radius: 25px;
+  background: #ffffff;
+  cursor: pointer;
+  -webkit-appearance: none;
+  margin-top: -3.5px;
+}
+input[type="range"]:focus::-webkit-slider-runnable-track {
+  background: #1db954;
+}
+input[type="range"]::-moz-range-track {
+  width: 100%;
+  height: 3px;
+  cursor: pointer;
+  box-shadow: 0px 0px 0px #000000;
+  background: #1db954;
+  border-radius: 50px;
+  border: 0px solid #000000;
+}
+input[type="range"]::-moz-range-thumb {
+  box-shadow: 0px 0px 0px #000000;
+  border: 1px solid #ffffff;
+  height: 9px;
+  width: 9px;
+  border-radius: 50px;
+  background: #ffffff;
+  cursor: pointer;
+}
+input[type="range"]::-ms-track {
+  width: 100%;
+  height: 3px;
+  cursor: pointer;
+  background: transparent;
+  border-color: transparent;
+  color: transparent;
+}
+input[type="range"]::-ms-fill-lower {
+  background: #1db954;
+  border: 0px solid #000000;
+  border-radius: 100px;
+  box-shadow: 0px 0px 0px #000000;
+}
+input[type="range"]::-ms-fill-upper {
+  background: #1db954;
+  border: 0px solid #000000;
+  border-radius: 100px;
+  box-shadow: 0px 0px 0px #000000;
+}
+input[type="range"]::-ms-thumb {
+  margin-top: 1px;
+  box-shadow: 0px 0px 0px #000000;
+  border: 1px solid #ffffff;
+  height: 9px;
+  width: 9px;
+  border-radius: 50px;
+  background: #ffffff;
+  cursor: pointer;
+}
+input[type="range"]:focus::-ms-fill-lower {
+  background: #1db954;
+}
+input[type="range"]:focus::-ms-fill-upper {
+  background: #1db954;
 }
 </style>
